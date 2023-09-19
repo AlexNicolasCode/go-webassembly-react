0584be4e24f97154c2d5054baf74b7d7d9e17e6c
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-18 21:34:24 -0300

    feat: localhost:3000 should be able to request wasm file when necessary

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
-const dir = "./src"
+const dir = "./src/presentation/wasm/static/"
 
 func main() {
-	fs := http.FileServer(http.Dir(dir))
-	log.Print("Serving " + dir + " on http://localhost:3000")
-	http.ListenAndServe(":3000", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		resp.Header().Add("Cache-Control", "no-cache")
-		if strings.HasSuffix(req.URL.Path, ".wasm") {
-			resp.Header().Set("content-type", "application/wasm")
-		}
-		fs.ServeHTTP(resp, req)
-	}))
+	r := mux.NewRouter()
+	r.HandleFunc("/{file}", fileHandle).Methods(http.MethodGet)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	})
+	handler := c.Handler(r)
+	fmt.Printf("Server running")
+	http.ListenAndServe(":8000", handler)
+}
+
+func fileHandle(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Cache-Control", "no-cache")
+	if strings.HasSuffix(req.URL.Path, ".wasm") {
+		file := mux.Vars(req)["file"]
+		res.Header().Set("content-type", "application/wasm")
+		data, _ := os.ReadFile(dir + file)
+		res.WriteHeader(http.StatusOK)
+		res.Write(data)
+	}
 }
