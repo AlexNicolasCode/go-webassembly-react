b5a61d8ae45381722fb0ae3abc27ab9d2e4cd108
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-19 09:26:11 -0300

    feat: set react initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client"
+
+import { HomePage } from "./presentation/pages";
+
+const root = createRoot(document.getElementById('root'));
+root.render(<HomePage/>);
+  
