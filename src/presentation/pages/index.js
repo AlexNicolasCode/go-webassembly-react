9efae96a2b5705367e4b409d0b6b3a0e4d004cb2
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-19 09:27:53 -0300

    feat: export path update

--- src/presentation/pages/index.js
+++ src/presentation/pages/index.js
@@ -1,10 +1,1 @@
-export const initWasm = async () => {
-    const go = new Go();
-    const { instance } = await WebAssembly.instantiateStreaming(
-        fetch("../../wasm/static/main.wasm"),
-        go.importObject,
-    )
-    go.run(instance)
-    const add = instance.exports.add;
-    return { add }
-}
+export * from "./home"
